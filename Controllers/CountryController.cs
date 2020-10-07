6fccc374c571055b077259c7e69c555684e5a4fb
Chung Hak Ngor <chunghakngor@gmail.com>
2020-10-07 20:15:43 +1300

    minor updates

--- Controllers/CountryController.cs
+++ Controllers/CountryController.cs
@@ -6,9 +6,11 @@ using System.Collections.Generic;
 namespace country_api.Controllers
 {
     [Route("api/[controller]")]
+    // Sets the route for the controller host:<port>/api/Country
     [ApiController]
-    public class CountryController : ControllerBase
+    public class CountryController : ControllerBase 
     {
+        // Calling the database connection service
         private readonly CountryService _countryService;
 
         public CountryController(CountryService countryService)
@@ -17,14 +19,17 @@ namespace country_api.Controllers
         }
 
         [HttpGet]
+        // Returns a list of all the country
         public ActionResult<List<Country>> Get() =>
             _countryService.Get();
 
         [HttpGet("{id:length(24)}", Name = "GetCountry")]
+        // Return a country from the Id
         public ActionResult<Country> Get(string id)
         {
+            // find the country in the database using the ID
             var country = _countryService.Get(id);
-
+            // if it doesn't exist then return not found
             if (country == null)
             {
                 return NotFound();
@@ -34,6 +39,7 @@ namespace country_api.Controllers
         }
 
         [HttpPost]
+        // POST route for a new country
         public ActionResult<Country> Create(Country country)
         {
             _countryService.Create(country);
@@ -42,6 +48,7 @@ namespace country_api.Controllers
         }
 
         [HttpPut("{id:length(24)}")]
+        // PUT Route for an existing country (requires the country ID)
         public IActionResult Update(string id, Country countryIn)
         {
             var country = _countryService.Get(id);
@@ -57,6 +64,7 @@ namespace country_api.Controllers
         }
 
         [HttpDelete("{id:length(24)}")]
+        // DEL Route from using country Id
         public IActionResult Delete(string id)
         {
             var country = _countryService.Get(id);
--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 
 Basic ASP.NET Web API for Basic Country Data 
 
+Requirements:
+```
+mongo
+dotnet
+```
+
 To run:
 ```
+cd country-api
 dotnet build country-api.csproj
 dotnet run country-api.csproj
 ```
 
+To edit mongodb connection:
+```
+1. Open appsettings.json
+2. Edit fields in COuntryDatabaseSettings
+```
+
+To edit host/port settings:
+```
+1. Open launchSettings.json
+2. Edit applicationUrl under country_api
 
 To access:
 ```
